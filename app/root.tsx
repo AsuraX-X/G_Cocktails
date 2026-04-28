0d9358c229bf7b8e54f4b661538bb148e97d7ef5
AsuraX-X <kiba741@gmail.com>
2026-04-28 16:46:52 +0000

    initialize project

--- app/root.tsx
+++ app/root.tsx
@@ -9,6 +9,10 @@ import {
 
 import type { Route } from "./+types/root";
 import "./app.css";
+import gsap from "gsap";
+import { ScrollTrigger, SplitText } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
